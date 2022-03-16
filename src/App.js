25e73aa2fcf7903ce85b6da2365867cc4c6d32da
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2022-03-16 02:15:02 +0200

    modify rabbit head width + add page overflow y & x as hidden

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
     <div className="container">
       <div className="rabbit-head">
         <div className="rabbit-ear"></div>
+        <div className="rabbit-ear-bottom-patch"></div>
+        <div className="rabbit-eye"></div>
+        <div className="rabbit-nose"> </div>
+        <div className="rabbit-mouse"> </div>
       </div>
+      <div className="rabbit-body"></div>
     </div>
   );
 }
